bb4dbcfb4c5a283a79cfddcaebfac442809af849
unknown <aisyahnovitasari220@gmail.com>
2025-04-30 13:50:07 +0700

    menambahkan tugas

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::create([
+            'nama'     => 'Aisyah Novitasari',
+            'email'    => 'admin@gmail.com',
+            'jabatan'  => 'Admin',
+            'password' => Hash::make('12345678'),
+            'is_tugas' => false,
+        ]);
+        User::create([
+            'nama'     => 'april',
+            'email'    => 'april@gmail.com',
+            'jabatan'  => 'Karyawan',
+            'password' => Hash::make('12345678'),
+            'is_tugas' => false,
+        
+        ]);
+    }
+}
